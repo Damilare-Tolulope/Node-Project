538a3638b4ff95bf94796bb1803ca5a8d55a2174
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2021-11-18 16:14:00 +0100

    Cart model added

--- models/Cart.js
+++ models/Cart.js
@@ -2,10 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const cartSchema = new Schema({
-    _id : String,
-    products : [String],
-    quantity : Number,
-    price : Number
+    userId : mongoose.Schema.Types.ObjectId,
+    products : [
+        {
+            productId : mongoose.Schema.Types.ObjectId,
+            quantity : {
+                type : Number,
+                default : 1,
+                min : [1, "Quantity cannot be less than 1."]
+            },
+            name : String,
+            price : Number
+        }
+    ],
+    active : {
+        type : Boolean,
+        default : true
+    },
+    modifiedOn : {
+        type : Date,
+        default : Date.now
+    },
+},{
+    timestamps : true
 })
 
 const Cart = mongoose.model("Cart", cartSchema)
